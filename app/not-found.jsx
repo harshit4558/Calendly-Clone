13584a516467aa3f4e7e94837237312d25b83cf9
harshit4558 <162314365+harshit4558@users.noreply.github.com>
2025-01-15 23:05:18 +0000

    updated not-found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
+
 export default function NotFound() {
     return (
+        <Suspense>
         <div className=" text0-4xl font-extrabold w-screen pt-80 grid justify-center">
             <h1>
                 404 - Page Not Found
             </h1>
         </div>
+        </Suspense>
     )
 }
